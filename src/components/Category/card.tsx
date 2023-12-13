d2ba440a3d868a95078b521d94b67a2440d48848
rgualberto-clear <ryangualberto.clear@gmail.com>
2023-12-13 18:14:14 -0300

    feat: diretores page

--- src/components/Category/card.tsx
+++ src/components/Category/card.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { IconProps } from "@phosphor-icons/react/"
+import Link from "next/link"
 
 interface CardProps {
   icon: React.FC<IconProps>
   title: string
   className?: string
   description: string
+  route: string
 }
 
 export default function Card({
@@ -13,9 +15,11 @@ export default function Card({
   title,
   description,
   className,
+  route,
 }: CardProps) {
   return (
-    <div
+    <Link
+      href={route}
       className={`group flex h-80 w-64 cursor-pointer flex-col items-center justify-center gap-14 rounded-lg border shadow-xl shadow-[#00000015] duration-300 ease-in-out hover:-translate-y-2  hover:bg-secondary ${className}`}
     >
       <Icon
@@ -29,6 +33,6 @@ export default function Card({
       <span className="absolute px-5 text-center text-xl font-medium text-white opacity-0 duration-300 ease-in group-hover:opacity-100">
         {description}
       </span>
-    </div>
+    </Link>
   )
 }
---
+++ src/mock/directors.ts
@@ -0,0 +1,44 @@
+import { IDirector } from "@/components/DirectorCard"
+
+export const directors: IDirector[] = [
+  {
+    image: "https://source.unsplash.com/random",
+    name: "Anthony Russo",
+    id: "testee-teste-1",
+    phone: "13 99999-9999",
+    email: "anthonyrusso@outlook.com",
+    socialMedia: {
+      linkedin: "https://www.linkedin.com/in/anthony-russo-6b0a9b1b/",
+    },
+  },
+  {
+    image: "https://source.unsplash.com/random",
+    name: "Rodofo Alves",
+    id: "testee-teste-1",
+    phone: "13 99999-9999",
+    email: "rodolfo@gmail.com",
+    socialMedia: {
+      linkedin: "https://www.linkedin.com/in/rodolfo-alves-6b0a9b1b/",
+    },
+  },
+  {
+    image: "https://source.unsplash.com/random",
+    name: "Israel Salazar",
+    id: "testee-teste-1",
+    phone: "13 99999-9999",
+    email: "salazar.producoes@outlook.com",
+    socialMedia: {
+      linkedin: "https://www.linkedin.com/in/israel-salazar-6b0a9b1b/",
+    },
+  },
+  {
+    image: "https://source.unsplash.com/random",
+    name: "Bruno Pereira",
+    id: "testee-teste-1",
+    phone: "13 99999-9999",
+    email: "pepito@bing.com",
+    socialMedia: {
+      linkedin: "https://www.linkedin.com/in/bruno-pereira-6b0a9b1b/",
+    },
+  },
+]
