2d4b830c561cf0cc7fe0695a147cb99564239dd6
Miguel B. <eomiguelbarros@gmail.com>
2023-12-13 21:36:51 -0300

    fix: fixing the responsiveness for page

--- src/app/statutes/page.tsx
+++ src/app/statutes/page.tsx
@@ -11,14 +11,14 @@ export default function Statutes() {
         title="Normas e Estatutos"
         subTitle="Consulte nossas normas e estatutos:"
       />
-      <div className="flex w-full">
+      <div className="flex w-full max-sm:flex-wrap">
         <div className="flex w-full flex-col gap-5">
           {regulations.map((regulation, index) => (
             <div className="flex items-center gap-5" key={index}>
-              <span className="relative flex h-[150px] w-[150px] cursor-pointer items-center justify-center rounded-full bg-primary">
+              <span className="flex h-[150px] w-[150px] cursor-pointer items-center justify-center rounded-full bg-primary">
                 <FilePdf className="h-5/6 w-11/12 text-white" />
               </span>
-              <div key={index} className="flex flex-col gap-1">
+              <div key={index} className="flex w-6/12 flex-col gap-1">
                 <span className="text-md font-medium text-primary">
                   {regulation.title}
                 </span>
@@ -30,7 +30,7 @@ export default function Statutes() {
           ))}
         </div>
         <Information.Grid className="gap-2">
-          <span className="mb-1 text-lg font-semibold text-primary">
+          <span className="mb-1 text-lg font-semibold text-primary max-sm:text-center">
             Sobre as taxas:
           </span>
           {fees.map((fee, index) => (
