f768488ff5b986ec35616a8dbdaa19d75c980092
Miguel <79284434+Miguel-Barros@users.noreply.github.com>
2023-12-13 21:39:15 -0300

    feat: creating statutes page (#10)
    * feat: creating statutes page
    
    * fix: fixing the responsiveness for page

---
+++ src/app/statutes/page.tsx
@@ -0,0 +1,47 @@
+import { Information } from "@/components/Information"
+import Title from "@/components/Title"
+import { fees, regulations } from "@/mock/statutes"
+import { FilePdf } from "@phosphor-icons/react/dist/ssr"
+import React from "react"
+
+export default function Statutes() {
+  return (
+    <main className="m-auto flex max-w-screen-xl flex-col items-center gap-5">
+      <Title
+        title="Normas e Estatutos"
+        subTitle="Consulte nossas normas e estatutos:"
+      />
+      <div className="flex w-full max-sm:flex-wrap">
+        <div className="flex w-full flex-col gap-5">
+          {regulations.map((regulation, index) => (
+            <div className="flex items-center gap-5" key={index}>
+              <span className="flex h-[150px] w-[150px] cursor-pointer items-center justify-center rounded-full bg-primary">
+                <FilePdf className="h-5/6 w-11/12 text-white" />
+              </span>
+              <div key={index} className="flex w-6/12 flex-col gap-1">
+                <span className="text-md font-medium text-primary">
+                  {regulation.title}
+                </span>
+                <span className="text-md font-medium text-secondary">
+                  Clique para baixar
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Information.Grid className="gap-2">
+          <span className="mb-1 text-lg font-semibold text-primary max-sm:text-center">
+            Sobre as taxas:
+          </span>
+          {fees.map((fee, index) => (
+            <Information.Text
+              key={index}
+              label={fee.label}
+              description={fee.description}
+            />
+          ))}
+        </Information.Grid>
+      </div>
+    </main>
+  )
+}
---
+++ src/mock/statutes.ts
@@ -0,0 +1,73 @@
+export const regulations = [
+  {
+    title: "Regulamentação sobre a PESCA",
+    link: "",
+  },
+  {
+    title: "Regulamentação sobre o LANÇAMENTO",
+    link: "",
+  },
+  {
+    title: "Regulamentação sobre a FPPL",
+    link: "",
+  },
+]
+
+export const fees = [
+  {
+    label: "Renovação de inscrição:",
+    description: "R$45,00",
+  },
+  {
+    label: "Inscrição de novos atletas desconto de 50%:",
+    description: "R$22,50",
+  },
+  {
+    label: "Inscrição individual Campeonato de Pesca:",
+    description: "R$260,00",
+  },
+  {
+    label: "Inscrição individual Campeonato de Lançamento Masculino:",
+    description: "R$270,00",
+  },
+  {
+    label: "Inscrição individual Campeonato de Lançamento Feminino:",
+    description: "R$180,00",
+  },
+  {
+    label: "Inscrição interclubes Pesca(Unico):",
+    description: "R$655,00",
+  },
+  {
+    label: "Inscrição interclubes Lançamento(Unico):",
+    description: "R$360,00",
+  },
+  {
+    label: "Inscrição individual Campeonato Long Casting:",
+    description: "R$135,00",
+  },
+  {
+    label: "Inscrição Juvenil Pesca e Lançamento:",
+    description: "ISENTO",
+  },
+  {
+    label: "Transferência de atleta:",
+    description: "R$180,00",
+  },
+  {
+    label: "Recurso a FPPL",
+    description: "(3) Três salários mínimos:",
+  },
+  {
+    label: "Certificado - Torneio de pesca - renda bruta até R$4.500,00:",
+    description: "R$360,00",
+  },
+  {
+    label: "Certificado/Alvará - Torneios pesca embarcada:",
+    description: "R$950,00",
+  },
+  {
+    label: "Certificado - Torneios de pesca - Renda bruta acima de R$4.500,00:",
+    description: "7,5% da Renda Bruta",
+  },
+]
